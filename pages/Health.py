89be1a2a7fad9ef1b4e63f009a919801b1e3be56
Tirtha2107 <tirthakadam63@gmail.com>
2026-03-19 19:40:12 +0530

    pssmaq

--- pages/Health.py
+++ pages/Health.py
@@ -32,9 +32,11 @@ def health_page():
     # -----------------------------
     st.markdown("""
     <style>
+
     .stApp {
         background-color: #FF77B1;
     }
+
     .top-banner {
         width: 100%;
         height: 300px;
@@ -42,6 +44,7 @@ def health_page():
         position: relative;
         padding-left: 40px;
     }
+
     .girl-img {
         position: absolute;
         right: 50px;
@@ -49,34 +52,97 @@ def health_page():
         width: 23vw;
         max-width: 500px;
     }
+
+    /* TITLE (Laptop) */
     .title {
         font-size: 100px;
         font-weight: 900;
         color: white;
-    
     }
-    @media (max-width: 768px) {
 
-        .title {
-            font-size: 55px;
-            text-align: center;
-        }
+    /* SUBTITLE (Laptop) */
     .subtitle {
         font-size: 30px;
         font-weight: 800;
         color: white;
     }
-                
-    /* MOBILE VERSION */
-    @media (max-width: 768px) {
-        .subtitle {
-            font-size: 18px;
-            text-align: center;
+
+    /* ---------- MOBILE VERSION ---------- */
+    @media screen and (max-width: 768px) {
+
+        .title {
+            font-size: 55px !important;
+            text-align: center !important;
         }
-                
+
+        .subtitle {
+            font-size: 18px !important;
+            text-align: center !important;
+        }
+
+        .girl-img {
+            position: relative;
+            right: 0;
+            top: 0;
+            width: 60%;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: 10px;
+        }
+
+    }
 
     </style>
     """, unsafe_allow_html=True)
+    # st.markdown("""
+    # <style>
+    # .stApp {
+    #     background-color: #FF77B1;
+    # }
+    # .top-banner {
+    #     width: 100%;
+    #     height: 300px;
+    #     background: #FF77B1;
+    #     position: relative;
+    #     padding-left: 40px;
+    # }
+    # .girl-img {
+    #     position: absolute;
+    #     right: 50px;
+    #     top: 10px;
+    #     width: 23vw;
+    #     max-width: 500px;
+    # }
+    # .title {
+    #     font-size: 100px;
+    #     font-weight: 900;
+    #     color: white;
+    
+    # }
+    # @media (max-width: 768px) {
+
+    #     .title {
+    #         font-size: 55px;
+    #         text-align: center;
+    #     }
+    # }
+    # .subtitle {
+    #     font-size: 30px;
+    #     font-weight: 800;
+    #     color: white;
+    # }
+                
+    # /* MOBILE VERSION */
+    # @media (max-width: 768px) {
+    #     .subtitle {
+    #         font-size: 18px;
+    #         text-align: center;
+    #     }
+    # }   
+
+    # </style>
+    # """, unsafe_allow_html=True)
 
     # -----------------------------
     # TOP BANNER
